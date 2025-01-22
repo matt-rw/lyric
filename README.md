9a51a6fb39dafc2d6206d36fc6fba098b6e338cc
Matthew Wear <mrwear12@gmail.com>
2025-01-22 13:54:47 -0600

    command line tool for displaying or viewing song lyrics

---
+++ README.md
@@ -0,0 +1,24 @@
+## Lyrics
+
+## Installation
+
+```bash
+$ python3 -m venv .venv
+$ source .venv/bin/activate
+$ pip install -r requirements.txt
+```
+
+## Usage
+
+Run from the command line with the artist name and song title.
+
+### Example
+
+```bash
+$ python3 find.py "Creedence Clearwater Revival" "Green River"
+```
+
+```bash
+$ python3 find.py --title --file "Miles Davis" "So What"
+Saved lyrics to 'milesdavis-sowhat.txt'
+```
---
+++ find.py
@@ -0,0 +1,78 @@
+import argparse
+import os
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_lyrics(artist: str, song: str) -> str | None:
+    """Fetch lyrics with HTTP request.
+
+    :param artist: Artist name with no spaces or caps.
+    :param song: Song name with no spaces or caps.
+
+    :return: String of lyrics if found, otherwise None.
+    """
+    base_url = 'https://www.azlyrics.com/lyrics/'
+
+    source = requests.get(f'{base_url}/{artist}/{song}.html')
+    soup = BeautifulSoup(source.text, 'html.parser')
+    try:
+        lyrics = soup.find('b').find_next('b').find_next('div')
+    except AttributeError:
+        return None
+
+    return lyrics.get_text()
+
+
+def parse_args() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description=(
+            'Tool to display song lyrics in a tty.\n\n'
+            'examples:\n'
+            '  find.py "Creedence Clearwater Revival" "Green River"\n'
+            '  find.py creedenceclearwaterrevival greenriver\n'
+        ),
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        'artist',
+        help='artist name'
+    )
+    parser.add_argument('song', help='song name')
+    parser.add_argument(
+        '-f',
+        '--file',
+        action='store_true',
+        help='save lyrics to text file'
+    )
+    parser.add_argument(
+        '-t',
+        '--title',
+        action='store_true',
+        help='add title of artist and song, as provided'
+    )
+    args = parser.parse_args()
+    
+    args.f_artist = args.artist.lower().replace(' ', '')
+    args.f_song = args.song.lower().replace(' ', '')
+    
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    lyrics = get_lyrics(args.f_artist, args.f_song)
+    if not lyrics:
+        print(f'Failed to find \'{args.song}\' by {args.artist}')
+        sys.exit(1)
+    if args.title:
+        lyrics = f'{args.song} - {args.artist}\n{lyrics}'
+    if args.file:
+        file_path = f'{args.f_artist}-{args.f_song}.txt'
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(lyrics)
+            print(f'Saved lyrics to \'{os.path.abspath(file.name)}\'')
+    else:
+        print(lyrics)
